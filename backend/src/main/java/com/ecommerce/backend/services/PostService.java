bf820294dfcea9662288fa82611eac980ea63003
tranvanchung1409 <tranvanchung1409@gmail.com>
2023-12-05 01:20:57 +0700

    Update Data Get Post and Create getCategoryById

--- backend/src/main/java/com/ecommerce/backend/services/PostService.java
+++ backend/src/main/java/com/ecommerce/backend/services/PostService.java
@@ -2,12 +2,13 @@ package com.ecommerce.backend.services;
 
 import com.ecommerce.backend.domain.dto.PostDTO;
 import com.ecommerce.backend.domain.enums.FilterSortUser;
+import com.ecommerce.backend.domain.models.Post;
 import org.springframework.data.domain.Page;
 
 public interface PostService {
     void  createNewPostByUser(PostDTO postDTO);
-    Page<PostDTO> getPagingPost( Integer pageNo, Integer pageSize);
-    Page<PostDTO> getPagingPostFilter(Long category, Integer sortBy, FilterSortUser filterSortUser, Integer pageNo, Integer pageSize);
+    Page<Post> getPagingPost(Integer pageNo, Integer pageSize);
+    Page<Post> getPagingPostFilter(Long category, Integer sortBy, FilterSortUser filterSortUser, Integer pageNo, Integer pageSize);
     PostDTO getPostById(Long id);
     void updatePost(Long id, PostDTO postDTO);
     void lockPost(Long id);
